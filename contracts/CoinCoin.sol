311545370cd02ed0b7acbb06d4fa509abd578998
Dzheremilz <trempejeremie@gmail.com>
2021-05-22 15:10:29 +0200

    update

--- contracts/CoinCoin.sol
+++ contracts/CoinCoin.sol
@@ -2,36 +2,106 @@
 
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 contract CoinCoin {
-  mapping(address => uint256) private _balances;
-  string private _name;
-  string private _symbol;
-  uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    string private _name;
+    string private _symbol;
+    uint256 private _totalSupply;
 
-  event Transfer(address indexed sender, address indexed receipient, uint256 amount);
+    event Transfer(
+        address indexed sender,
+        address indexed receipient,
+        uint256 amount
+    );
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 amount
+    );
 
-  constructor(address owner_, uint256 totalSupply_) {
-    _name = "CoinCoin";
-    _symbol = "COIN";
-    _balances[owner_] = totalSupply_;
-    emit Transfer(address(0), owner_, totalSupply_);
-  }
-  
-  function name() public view returns (string memory) {
-    return _name;
-  }
+    constructor(address owner_, uint256 totalSupply_) {
+        _name = "CoinCoin";
+        _symbol = "COIN";
+        _balances[owner_] = totalSupply_;
+        emit Transfer(address(0), owner_, totalSupply_);
+    }
 
-  function symbol() public view returns (string memory) {
-    return _symbol;
-  }
+    function name() public view returns (string memory) {
+        return _name;
+    }
 
-  function totalSupply() public view returns (uint256) {
-    return _totalSupply;
-  }
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
 
-  function balanceOf(address account) public view returns (uint256) {
-    return _balances[account];
-  }
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
 
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
 
+    function transfer(address to, uint256 amount) external {
+        _transfer(msg.sender, to, amount);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public {
+        uint256 currentAllowance = _allowances[from][msg.sender];
+        _transfer(from, to, amount);
+        require(
+            currentAllowance >= amount,
+            "CoinCoin: transfer amount exceeds allowance"
+        );
+        _approve(from, msg.sender, currentAllowance - amount);
+    }
+
+    function approve(address spender, uint256 amount) public {
+        _approve(msg.sender, spender, amount);
+    }
+
+    function allowance(address owner, address spender)
+        public
+        view
+        returns (uint256)
+    {
+        return _allowances[owner][spender];
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) private {
+        require(
+            sender != address(0),
+            "CoinCoin: transfer from the zero address"
+        );
+        require(
+            recipient != address(0),
+            "CoinCoin: transfer to the zero address"
+        );
+        require(_balances[sender] >= amount, "CoinCoin: Not enough CoinCoin");
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) private {
+        require(owner != address(0), "CoinCoin: approve from the zero address");
+        require(spender != address(0), "CoinCoin: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
 }
--- test/CoinCoin-test.js
+++ test/CoinCoin-test.js
@@ -13,10 +13,8 @@ describe('CoinCoin Token', function () {
     await coincoin.deployed()
     /*
     Il faudra transférer des tokens à nos utilisateurs de tests lorsque la fonction transfer sera implementé
-    await coincoin
-      .connect(owner)
-      .transfer(alice.address, ethers.utils.parseEther('100000000'))
-      */
+    */
+    await coincoin.connect(owner).transfer(alice.address, ethers.utils.parseEther('100000000'))
   })
 
   describe('Deployement', function () {
@@ -36,24 +34,99 @@ describe('CoinCoin Token', function () {
 
     it('emits event Transfer when mint totalSupply', async function () {
       /*
-      Pb de récupération de l'event d'une transaction passée avec Waffle: Sofiane s'en occupe
-      await expect(
-        CoinCoin.connect(dev).deploy(
-          owner.address,
-          ethers.utils.parseEther('8000000000')
-        )
-      ).to.emit(, 'Transfer').withArgs(ethers.constants.AddressZero, owner.address, ethers.utils.parseEther('8000000000'));
+        On peut tester si un event a été emit depuis une transaction particulière.
+        Le problème c'est qu'une transaction de déploiement ne nous retourne pas la transaction
+        mais l'instance du smart contract déployé.
+        Pour récupérer la transaction qui déployé le smart contract il faut utilisé un l'attribut
+        ".deployTransaction" sur l'instance du smart contract
       */
+      let receipt = await coincoin.deployTransaction.wait()
+      let txHash = receipt.transactionHash
+      await expect(txHash)
+        .to.emit(coincoin, 'Transfer')
+        .withArgs(ethers.constants.AddressZero, owner.address, INITIAL_SUPPLY)
     })
   })
 
   describe('Allowance system', function () {
-    // Tester le système d'allowance ici
+    it('allow a spender to use owner tokens', async function () {
+      await coincoin.connect(alice).approve(eve.address, 100000)
+      expect(await coincoin.allowance(alice.address, eve.address)).to.equal(100000)
+    })
+
+    it('approve emits event', async () => {
+      await expect(coincoin.approve(eve.address, 1000))
+        .to.emit(coincoin, 'Approval')
+        .withArgs(dev.address, eve.address, 1000)
+    })
+    /*
+    it('should revert: from zero address', async function () {
+      await expect(coincoin.connect(ethers.constants.AddressZero).approve(eve.address, 100000)).to.be.revertedWith('CoinCoin: approve from the zero address')
+    })
+    */
+
+    it('should revert: to zero address', async function () {
+      await expect(coincoin.approve(ethers.constants.AddressZero, 10000)).to.be.revertedWith('CoinCoin: approve to the zero address')
+    })
   })
+
   describe('Token transfers', function () {
-    it('transfers tokens from sender to receipient', async function () {})
-    it('transferFrom tokens from sender to receipient', async function () {})
-    it('emits event Transfer when transfer token', async function () {})
-    it('emits event Transfer when transferFrom token', async function () {})
+    it('transfers tokens from sender to receipient', async function () {
+      await coincoin.connect(owner).transfer(eve.address, ethers.utils.parseEther('1000000'))
+      expect(await coincoin.balanceOf(eve.address)).to.equal(ethers.utils.parseEther('1000000'))
+    })
+
+    it('emits event Transfer when transfer token', async function () {
+      await expect(coincoin.connect(owner).transfer(eve.address, ethers.utils.parseEther('1000000')))
+        .to.emit(coincoin, 'Transfer')
+        .withArgs(owner.address, eve.address, ethers.utils.parseEther('1000000'))
+    })
+
+    it('should revert: to zero address', async function () {
+      await expect(coincoin.transfer(ethers.constants.AddressZero, 10000)).to.be.revertedWith('CoinCoin: transfer to the zero address')
+    })
+
+    it('should revert: not enough Token', async function () {
+      await expect(coincoin.transfer(eve.address, 10000)).to.be.revertedWith('CoinCoin: Not enough CoinCoin')
+    })
+
+    it('transferFrom tokens from sender to receipient', async function () {
+      await coincoin.connect(alice).approve(eve.address, 100000)
+      await coincoin.connect(eve).transferFrom(alice.address, dan.address, 10000)
+      expect(await coincoin.balanceOf(dan.address)).to.be.equal(10000)
+    })
+
+    it('emits event Transfer when transferFrom token', async function () {
+      await coincoin.connect(alice).approve(eve.address, 100000)
+      await expect(coincoin.connect(eve).transferFrom(alice.address, dan.address, 10000))
+        .to.emit(coincoin, 'Transfer')
+        .withArgs(alice.address, dan.address, 10000)
+    })
+
+    it('emits event Approval when transferFrom token', async function () {
+      await coincoin.connect(alice).approve(eve.address, 100000)
+      await expect(coincoin.connect(eve).transferFrom(alice.address, dan.address, 10000))
+        .to.emit(coincoin, 'Approval')
+        .withArgs(alice.address, eve.address, 100000 - 10000)
+    })
+
+    it('should revert: from zero address', async function () {
+      await expect(coincoin.connect(eve).transferFrom(ethers.constants.AddressZero, alice.address, 0)).to.be.revertedWith('CoinCoin: transfer from the zero address')
+    })
+
+    it('should revert: to zero address', async function () {
+      await coincoin.connect(alice).approve(eve.address, 100000)
+      await expect(coincoin.connect(eve).transferFrom(alice.address, ethers.constants.AddressZero, 10000)).to.be.revertedWith('CoinCoin: transfer to the zero address')
+    })
+
+    it('should revert: not enough token', async function () {
+      await coincoin.connect(dan).approve(eve.address, 100000)
+      await expect(coincoin.connect(eve).transferFrom(dan.address, charlie.address, 10000)).to.be.revertedWith('CoinCoin: Not enough CoinCoin')
+    })
+
+    it('should revert: transfer amount exceeds allowance', async function () {
+      await coincoin.connect(alice).approve(eve.address, 100000)
+      await expect(coincoin.connect(eve).transferFrom(alice.address, charlie.address, 1000000)).to.be.revertedWith('CoinCoin: transfer amount exceeds allowance')
+    })
   })
 })
